4e9b4f40c980014de99b847675750db344c09116
Raghu K <raghuk.vit@gmail.com>
2023-02-27 06:11:09 +0530

    Update tf-module-rabbitmq/

--- main.tf
+++ main.tf
@@ -59,6 +59,12 @@ resource "aws_mq_broker" "rabbitmq" {
 
 }
 
+resource "aws_ssm_parameter" "rabbitmq_endpoint" {
+  name  = "${var.env}.rabbitmq.ENDPOINT"
+  type  = "String"
+  value = aws_mq_broker.rabbitmq.instances.0.endpoints.0
+}
+
 
 
 
